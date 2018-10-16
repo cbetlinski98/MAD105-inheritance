a203b083addc96cf16a93415f50256fcf45b6e08
cbetlinski98 <cbetlinski@students.mchenry.edu>
2018-10-16 00:11:02 -0500

    Inheritance Assignment

---
+++ Inheritance/src/com/example/java/Customer.java
@@ -0,0 +1,47 @@
+package com.example.java;
+
+public class Customer {
+    String customerName;
+    String customerPhone;
+    String customerAddress;
+    double squareFootage;
+
+    public Customer(String customerName, String customerPhone, String customerAddress, double squareFootage) {
+        this.customerName = customerName;
+        this.customerPhone = customerPhone;
+        this.customerAddress = customerAddress;
+        this.squareFootage = squareFootage;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public String getCustomerPhone() {
+        return customerPhone;
+    }
+
+    public String getCustomerAddress() {
+        return customerAddress;
+    }
+
+    public double getSquareFootage() {
+        return squareFootage;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public void setCustomerPhone(String customerPhone) {
+        this.customerPhone = customerPhone;
+    }
+
+    public void setCustomerAddress(String customerAddress) {
+        this.customerAddress = customerAddress;
+    }
+
+    public void setSquareFootage(double squareFootage) {
+        this.squareFootage = squareFootage;
+    }
+}
---
+++ Inheritance/src/com/example/java/Residential.java
@@ -0,0 +1,30 @@
+package com.example.java;
+
+public class Residential extends Customer {
+    boolean senior;
+    double total;
+
+    public Residential(String customerName, String customerPhone, String customerAddress, double squareFootage, boolean senior) {
+        super(customerName, customerPhone, customerAddress, squareFootage);
+    }
+
+    public Residential(String name, String phone, String address, Double footage) {
+        super(name, phone, address, footage);
+    }
+
+    public boolean getSenior(){
+        return senior;
+    }
+
+    public void setSenior(boolean senior){
+        this.senior = senior;
+    }
+
+    public double seniorDiscount(boolean senior) {
+        if (senior)
+            total = ((squareFootage * 6) / 1000) * 0.85;
+        else
+            total = (squareFootage * 6) / 1000;
+        return total;
+    }
+}
